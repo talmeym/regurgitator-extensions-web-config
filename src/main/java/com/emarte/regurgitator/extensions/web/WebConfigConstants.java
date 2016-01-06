54262784e83c878bb52b0a313b68a82cfdf9f994
talmeym <mtalmey@gmail.com>
2016-01-06 16:06:44 +0000

    added username and password authentication to http call step

--- src/main/java/com/emarte/regurgitator/extensions/web/WebConfigConstants.java
+++ src/main/java/com/emarte/regurgitator/extensions/web/WebConfigConstants.java
@@ -3,5 +3,7 @@ package com.emarte.regurgitator.extensions.web;
 public class WebConfigConstants {
 	public static final String HOST = "host";
 	public static final String PORT = "port";
+	public static final String USERNAME = "username";
+	public static final String PASSWORD = "password";
 	public static final String PROCESS_RESPONSE = "process-response";
 }
